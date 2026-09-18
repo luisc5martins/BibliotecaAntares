2ef76edd8b15db29d95916e8c22e3f03d9044b9a
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-18 14:02:41 -0300

    feat: Conectando a API ao frontend com Vue 3

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from core.views import CategoriaViewSet, UserViewSet
 router = DefaultRouter()
 
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
-router.register(r'users', UserViewSet, basename='users')
+router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
