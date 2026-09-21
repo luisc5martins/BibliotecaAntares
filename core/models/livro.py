868c70e018d9db4ac495fd3463588b1684a308db
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-21 13:22:39 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -13,6 +13,7 @@ class Livro(models.Model):
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.DISPONIVEL)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros', blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.status})'
