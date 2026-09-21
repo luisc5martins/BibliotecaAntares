83a641df592b908cfe4409a763b72ca0e21c1518
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-21 14:42:50 -0300

    feat: criação de múltiplos serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,4 @@ from .user import UserRegistrationSerializer, UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
-from .livro import LivroListRetrieveSerializer, LivroSerializer
+from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
