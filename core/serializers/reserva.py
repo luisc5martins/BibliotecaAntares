df0011cf40a22865022d6361289c5d971270263f
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-23 09:07:58 -0300

    feat: inclusão do e-mail do usuário na listagem da reserva

--- core/serializers/reserva.py
+++ core/serializers/reserva.py
@@ -1,8 +1,8 @@
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework.serializers import CharField, ModelSerializer
 from core.models import Reserva
 
 class ReservaSerializer(ModelSerializer):
+    usuario = CharField(source='usuario.email', read_only=True)
     class Meta:
         model = Reserva
         fields = '__all__'
