7506a65a4b0a7d6994d0c7609ff7ea15a555becc
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-23 09:08:37 -0300

    feat: inclusão do status da reserva na listagem da reserva

--- core/serializers/reserva.py
+++ core/serializers/reserva.py
@@ -3,6 +3,7 @@ from core.models import Reserva
 
 class ReservaSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
+    status = CharField(source='get_status_display', read_only=True)
     class Meta:
         model = Reserva
         fields = '__all__'
