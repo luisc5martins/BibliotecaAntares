a959821cd69a9288991ee5608f409c6a86ff6120
luisc5martins <luisc.martinsjunior5@gmail.com>
2026-09-23 09:11:27 -0300

    feat: visualização dos itens da reserva na listagem da reserva

--- core/serializers/reserva.py
+++ core/serializers/reserva.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import CharField, ModelSerializer
-from core.models import Reserva
+from core.models import Reserva, ItensReserva
+
+class ItensReservaSerializer(ModelSerializer):
+    class Meta:
+        model = ItensReserva
+        fields = '__all__'
 
 class ReservaSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True)
+    itens = ItensReservaSerializer(many=True, read_only=True)
     class Meta:
         model = Reserva
         fields = '__all__'
